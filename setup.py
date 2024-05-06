0137b90abf40697460787b4d436df44328daeb2a
benalieur <benjamin.quinet59b@gmail.com>
2024-05-06 10:24:59 +0200

    docs: :memo: add docstring

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="code_to_text",
     version="0.1.0",
@@ -7,7 +8,6 @@ setup(
     author_email="benjamin.quinet59b@gmail.com",
     description="A Python package to concatenate code files into a single text file.",
     long_description=open('readme.md').read(),
-    long_description_content_type='text/html',
     url="https://github.com/Benalieur/CODE_TO_TEXT.git",
     packages=find_packages(),
     classifiers=[
